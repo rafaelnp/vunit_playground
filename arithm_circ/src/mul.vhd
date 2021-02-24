71745191de7988f3a28662d521bd34cecd30a849
Rafael do Nascimento Pereira <rnp@25ghz.net>
2021-02-24 21:25:41 +0100

    add multiplication initial code

--- arithm_circ/src/mul.vhd
+++ arithm_circ/src/mul.vhd
@@ -2,20 +2,37 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.numeric_std_unsigned.all;
+
 
 
 entity mul is
 	port(
-		i_clk  : in  std_logic;
-		i_rst  : in  std_logic;
-		i_en   : in  std_logic;
-		i_data : in  std_logic_vector(15 downto 0);
-		o_done : out std_logic;
-		o_data : out std_logic_vector(15 downto 0)
+		i_x    : in  std_logic_vector(15 downto 0);
+		i_y    : in  std_logic_vector(15 downto 0);
+		--o_done : out std_logic;
+		o_z    : out std_logic_vector(31 downto 0);
+		o_cout : out  std_logic
 	);
 end entity mul;
 
 architecture rtl of mul is
 begin
+	multi: process(i_x, i_y)
+		variable pv : std_logic_vector(31 downto 0) := (others => '0');
+		variable bp : std_logic_vector(31 downto 0) := (others => '0');
+	begin
+		bp := X"0000" & i_y;
+
+		for i in 0 to 15 loop
+			if i_x(i) ='1' then
+				pv := pv + bp;
+			end if;
+
+			bp := bp(30 downto 0) & '0';
+		end loop;
+
+		o_z <= pv;
+	end process multi;
 
 end architecture rtl;
