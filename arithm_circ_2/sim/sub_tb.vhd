552f82ec0a0b3ddce580615ad4a71b12284d8740
Rafael do Nascimento Pereira <rnp@25ghz.net>
2021-03-08 22:32:09 +0100

    implement subtractor

--- arithm_circ_2/sim/sub_tb.vhd
+++ arithm_circ_2/sim/sub_tb.vhd
@@ -1,8 +1,7 @@
-
-
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.numeric_std_unsigned.all;
 
 
 library vunit_lib;
@@ -10,16 +9,68 @@ context vunit_lib.vunit_context;
 
 entity sub_tb is
 	generic (runner_cfg : string);
-end entity sub_tb ;
+end entity sub_tb;
 
 
 architecture rtl of sub_tb is
+	constant C_SIZE : natural := 16;
+
+	signal s_x, s_y, s_z: std_logic_vector(C_SIZE-1 downto 0);
 begin
+
+	dut: entity work.sub(rtl)
+	generic map(
+		N => C_SIZE
+	)
+	port map(
+		i_x    => s_x,
+		i_y    => s_y,
+		o_z    => s_z
+	);
+
+	s_x <= (others => '0'),
+		std_logic_vector(to_unsigned(729, C_SIZE)) after 5 ns,
+		std_logic_vector(to_unsigned(14, C_SIZE)) after 10 ns,
+		std_logic_vector(to_unsigned(322, C_SIZE)) after 20 ns,
+		std_logic_vector(to_unsigned(4032, C_SIZE)) after 30 ns;
+
+	s_y <= (others => '0'),
+		std_logic_vector(to_unsigned(314, C_SIZE)) after 5 ns,
+		std_logic_vector(to_unsigned(415 , C_SIZE)) after 10 ns,
+		std_logic_vector(to_unsigned(322 , C_SIZE)) after 20 ns,
+		std_logic_vector(to_unsigned(6129, C_SIZE)) after 30 ns;
 
 	main: process
 	begin
 		test_runner_setup(runner, runner_cfg);
+
+		wait for 7 ns;
+		info("first");
+		check_equal(signed(s_z), 729 - 314);
+		info(to_string(to_integer(signed(s_z))));
+
+		wait for 10 ns;
+
+		info("second");
+		--check_equal(to_integer(s_z), to_integer(14 - 415));
+		check_equal(signed(s_z), -401);
+		--check_equal(to_integer(signed(s_z)), to_integer(signed(14 - 415)));
+		info(to_string(to_integer(signed(s_z))));
+
+		wait for 10 ns;
+
+		info("third");
+		check_equal(s_z, 322 - 322);
+		info(to_string(to_integer(unsigned(s_z))));
+
+		wait for 20 ns;
+
+		info("fourth");
+		check_equal(signed(s_z), 4032 - 6129);
+		info(to_string(to_integer(signed(s_z))));
+
 		test_runner_cleanup(runner);
 	end process main;
 
 end architecture rtl;
+
--- arithm_circ_2/src/sub.vhd
+++ arithm_circ_2/src/sub.vhd
@@ -2,20 +2,29 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.numeric_std_unsigned.all;
 
 
 entity sub is
+	generic(
+		N : natural := 4
+	);
 	port(
-		i_clk  : in  std_logic;
-		i_rst  : in  std_logic;
-		i_en   : in  std_logic;
-		i_data : in  std_logic_vector(15 downto 0);
-		o_done : out std_logic;
-		o_data : out std_logic_vector(15 downto 0)
+		i_x    : in  std_logic_vector(N-1 downto 0);
+		i_y    : in  std_logic_vector(N-1 downto 0);
+		o_z    : out std_logic_vector(N-1 downto 0)
 	);
 end entity sub;
 
 architecture rtl of sub is
+	signal s_z : std_logic_vector(N-1 downto 0);
 begin
+
+	o_z <= s_z;
+
+	subb: process(i_x, i_y)
+	begin
+		s_z <= i_x - i_y;
+	end process subb;
 
 end architecture rtl;
